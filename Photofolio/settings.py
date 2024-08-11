d3697fbc0c7303040d4af7be94fbcd6ce8167247
Mohammad Junaid Alam <siddiquijunaid765@gmail.com>
2024-08-11 21:58:32 +0530

    Hosting Requirement

--- Photofolio/settings.py
+++ Photofolio/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)c-9cct6q9rjorzo6=zet9^+k$p9s@1xv_)jv)a(afz9mexk+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -87,7 +87,9 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-DATABASES["default"] = dj_database_url.parse("postgresql://photofolio_user:etwto8h50rh0qv2EFz8cMUsGRjXe3OUL@dpg-cqsarc5umphs73cu7n00-a.oregon-postgres.render.com/photofolio")
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 
